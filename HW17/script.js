618bf939bb4a1d95882eb0af89171d7e299fcbe5
StasShein <usastassh@mail.ru>
2022-10-12 20:50:45 +0300

    addHW17

---
+++ HW17/script.js
@@ -0,0 +1,15 @@
+console.log(document.getElementById('age-table'))
+
+let a = document.querySelectorAll('#age-table label')
+
+console.log(a)
+
+console.log(document.querySelector('tr > td:first-Child'))
+
+console.log(document.querySelector('form[name="search"]'))
+
+console.log(document.querySelector('form input:first-Child'))
+
+console.log(document.querySelector('form > input:last-Child'))
+
+
